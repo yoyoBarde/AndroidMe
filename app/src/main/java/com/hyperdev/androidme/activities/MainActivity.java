4d38365578d5b92db8eceb78be68cefbedf1c6d6
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2017-09-06 06:31:51 +0800

    Added image asset drawables and helper method to retrieve the set of android me images.

--- app/src/main/java/com/hyperdev/androidme/activities/MainActivity.java
+++ app/src/main/java/com/hyperdev/androidme/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.hyperdev.androidme;
+package com.hyperdev.androidme.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hyperdev.androidme.R;
 
 public class MainActivity extends AppCompatActivity {
 
