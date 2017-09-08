c7c41cf543995416206cddbf63c2817ce1c91fd5
Julius Barde <yam1434414344@gmail.com>
2017-09-08 23:55:39 +0800

    First Commit

---
+++ app/src/main/java/fragment/AndroidFragment.java
@@ -0,0 +1,28 @@
+package layout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hyperdev.androidme.R;
+
+
+public class AndroidFragment extends Fragment {
+
+
+    public AndroidFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.android_fragment, container, false);
+    }
+
+}
